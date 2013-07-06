0398275e824ab15a55f56e2ead8bd020ef78ae0e
Yoji SHIDARA <dara@shidara.net>
2013-07-06 20:04:06 +0900

    Require lib in spec_helper

--- spec/gimlet/data_store_spec.rb
+++ spec/gimlet/data_store_spec.rb
@@ -1,5 +1,4 @@
 require_relative '../spec_helper'
-require_relative '../../lib/gimlet'
 
 describe Gimlet::DataStore do
   describe 'empty' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require_relative '../lib/gimlet'
+
 def fixture_path(name)
   File.join(__dir__, '..', 'fixtures', name)
 end
