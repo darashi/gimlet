f4a9431bf7de1c93cd1e0d4d125a1d10cbdbb918
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-08-15 10:33:32 +0900

    Support to load data from filename

--- lib/gimlet/data_store.rb
+++ lib/gimlet/data_store.rb
@@ -14,21 +14,32 @@ module Gimlet
     end
 
     def load_all!
-      Pathname.glob(@data_directory + '**/*.{yaml,yml}').each do |path|
-        extension = path.extname
-        basename = path.basename(extension)
+      if File.directory?(@data_directory)
+        Pathname.glob(@data_directory + '**/*.{yaml,yml}').each do |path|
+          extension = path.extname
+          basename = path.basename(extension)
 
-        parts = path.relative_path_from(@data_directory).split.map(&:to_s)[0..-1]
-        parts.delete('.')
-        parts.pop
+          parts = path.relative_path_from(@data_directory).split.map(&:to_s)[0..-1]
+          parts.delete('.')
+          parts.pop
 
-        current = @local_data
-        parts.each do |part|
-          current[part] ||= ::Gimlet::Util.recursively_enhance({})
-          current = current[part]
+          current = @local_data
+          parts.each do |part|
+            current[part] ||= ::Gimlet::Util.recursively_enhance({})
+            current = current[part]
+          end
+
+          current[basename.to_s] = ::Gimlet::Util.recursively_enhance(YAML.load_file(path))
         end
+      else
+        %w(yaml yml).each do |ext|
+          path = @data_directory.sub_ext(".#{ext}")
 
-        current[basename.to_s] = ::Gimlet::Util.recursively_enhance(YAML.load_file(path))
+          if path.exist?
+            @local_data = ::Gimlet::Util.recursively_enhance(YAML.load_file(path))
+            break
+          end
+        end
       end
     end
 
