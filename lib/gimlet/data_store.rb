e0eefda6d608a9d932d8e2b2ccbe7390bc6309d7
Yoji SHIDARA <dara@shidara.net>
2013-08-15 10:46:15 +0900

    Extract load_from_file! and load_from_directory!

--- lib/gimlet/data_store.rb
+++ lib/gimlet/data_store.rb
@@ -15,30 +15,38 @@ module Gimlet
 
     def load_all!
       if File.directory?(@data_directory)
-        Pathname.glob(@data_directory + '**/*.{yaml,yml}').each do |path|
-          extension = path.extname
-          basename = path.basename(extension)
-
-          parts = path.relative_path_from(@data_directory).split.map(&:to_s)[0..-1]
-          parts.delete('.')
-          parts.pop
-
-          current = @local_data
-          parts.each do |part|
-            current[part] ||= ::Gimlet::Util.recursively_enhance({})
-            current = current[part]
-          end
-
-          current[basename.to_s] = ::Gimlet::Util.recursively_enhance(YAML.load_file(path))
-        end
+        load_from_directory!
       else
-        %w(yaml yml).each do |ext|
-          path = @data_directory.sub_ext(".#{ext}")
+        load_from_file!
+      end
+    end
 
-          if path.exist?
-            @local_data = ::Gimlet::Util.recursively_enhance(YAML.load_file(path))
-            break
-          end
+    def load_from_directory!
+      Pathname.glob(@data_directory + '**/*.{yaml,yml}').each do |path|
+        extension = path.extname
+        basename = path.basename(extension)
+
+        parts = path.relative_path_from(@data_directory).split.map(&:to_s)[0..-1]
+        parts.delete('.')
+        parts.pop
+
+        current = @local_data
+        parts.each do |part|
+          current[part] ||= ::Gimlet::Util.recursively_enhance({})
+          current = current[part]
+        end
+
+        current[basename.to_s] = ::Gimlet::Util.recursively_enhance(YAML.load_file(path))
+      end
+    end
+
+    def load_from_file!
+      %w(yaml yml).each do |ext|
+        path = @data_directory.sub_ext(".#{ext}")
+
+        if path.exist?
+          @local_data = ::Gimlet::Util.recursively_enhance(YAML.load_file(path))
+          break
         end
       end
     end
